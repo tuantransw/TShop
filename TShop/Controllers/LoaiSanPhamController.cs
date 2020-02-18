68a979708c6f5ede0df0abec83fd7f446089e3c0
TuanAnh <tuantran.sw@gmail.com>
2020-02-18 12:18:42 +0700

    Fix category

--- TShop/Controllers/LoaiSanPhamController.cs
+++ TShop/Controllers/LoaiSanPhamController.cs
@@ -26,11 +26,14 @@ namespace TShop.Controllers
             ViewBag.SoLuong = DanhMucSanPhamCon.Count();
             return PartialView(DanhMucSanPhamCon);
         }
-        public ActionResult DanhMucSanPhamTheoNSXPartial(int MaLSP)
+        public ActionResult DanhMucSanPhamTheoNSXPartial(int? MaLSP)
         {
-            var DanhMucSanPhamTheoNSX = db.SANPHAMs.Where(n=>n.MaLSP==MaLSP).ToList();
-           
-            return PartialView(DanhMucSanPhamTheoNSX);
+            var DanhMucSanPhamTheoNSX = db.SANPHAMs.Where(n => n.MaLSP == 8).ToList();
+            //var DanhMucSanPhamTheoNSX = db.SANPHAMs.Where(n => n.MaLSP == MaLSP).GroupBy(n => n.MaNSX).ToList();
+            var sPCha = from sP in db.SANPHAMs join lSP in db.LOAISANPHAMs on sP.MaLSP equals lSP.MaLSP where sP.Xoa == false && lSP.MaLSPGoc == MaLSP select sP;
+            ViewBag.MaLSP = MaLSP;
+            var sPCha1 = sPCha.ToList();
+            return PartialView(sPCha);
         }
 
 
--- TShop/Controllers/SanPhamController.cs
+++ TShop/Controllers/SanPhamController.cs
@@ -33,12 +33,55 @@ namespace TShop.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
             }
-            var sp = db.SANPHAMs.Where(n => n.MaLSP == MaLSP && n.Xoa == false).ToList();
-            if (sp == null)
+            var sp = db.LOAISANPHAMs.Where(n => n.MaLSP == MaLSP).ToList();
+            //var sPCon = db.SANPHAMs.Where(n => n.MaLSP == MaLSP).ToList();
+            var sPCha = from sP in db.SANPHAMs join lSP in db.LOAISANPHAMs on sP.MaLSP equals lSP.MaLSP where sP.Xoa == false && lSP.MaLSPGoc == MaLSP select sP;
+            foreach (var item in sp)
+            {
+                if (item.MaLSPGoc == null)
+                {
+                    
+                    sPCha = from sP in db.SANPHAMs join lSP in db.LOAISANPHAMs on sP.MaLSP equals lSP.MaLSP where sP.Xoa == false && lSP.MaLSPGoc == MaLSP select sP;
+                }
+                else
+                {
+                    sPCha = from sP in db.SANPHAMs join lSP in db.LOAISANPHAMs on sP.MaLSP equals lSP.MaLSP where sP.Xoa == false && lSP.MaLSP == MaLSP select sP;
+                    //sPCha = db.SANPHAMs.Where(n => n.MaLSP == MaLSP && n.Xoa == false).ToList();
+                }
+            }
+            //var sp1 = db.SANPHAMs.Where(n => n.MaLSP == MaLSP);
+            //foreach(var item in sp1)
+            //{
+            //    item.
+            //}
+            //if(sp1)
+            
+            
+            if (sPCha == null)
             {
                 return HttpNotFound();
             }
-            return View(sp);
+            return View(sPCha);
+        }
+        public ActionResult SanPhamTheoLSPVaNSX(int? MaLSP,int? MaNSX)
+        {
+            if (MaLSP == null||MaNSX == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            var sP1 = from sP in db.SANPHAMs join lSP in db.LOAISANPHAMs on sP.MaLSP equals lSP.MaLSP where sP.Xoa == false && lSP.MaLSPGoc == MaLSP && sP.MaNSX==MaNSX select sP;
+            
+            //var sp = db.SANPHAMs.Where(n => n.MaLSP == MaLSP &&n.MaNSX == MaNSX && n.Xoa == false).ToList();
+
+
+            //var Test = db.SANPHAMs.Where(n => n.MaLSP == MaLSP).ToList();
+            //Test.GroupBy(n=>n.)
+            if (sP1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sP1);
         }
     }
 }
